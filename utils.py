409e48e12365cac5bddb40172012ac664bd0fbd5
Erik Tollerud <erik.tollerud@gmail.com>
2019-06-09 23:13:10 -0400

    add utils

---
+++ utils.py
@@ -0,0 +1,26 @@
+import math
+import utime
+from machine import Pin, ADC
+
+# https://docs.espressif.com/projects/esp-idf/en/latest/api-reference/peripherals/adc.html says 0 dB is 1.1 V
+ADC_to_voltage = lambda adc, attendb=0: 1.1*10**(attendb/20)*adc.read()/4095
+
+def read_battery(pin=35):
+    adc = ADC(Pin(pin))
+    adc.atten(ADC.ATTN_11DB)
+    return 2*ADC_to_voltage(adc, 11)
+
+def piezo_multitone(pwm, freqs, n_waves, timeout_ms):
+    if timeout_ms is None:
+        timeout_ms = float('inf')
+
+    minfreq = min(freqs)
+    sleep_ms = math.ceil(n_waves*1000/minfreq)
+
+    start = utime.ticks_ms()
+    while utime.ticks_diff(utime.ticks_ms(), start) < timeout_ms:
+        for f in freqs:
+            pwm.freq(f)
+            utime.sleep_ms(sleep_ms)
+
+    pwm.deinit()
